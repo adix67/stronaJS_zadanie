02aaff4c7197291ad9faa008c63523cb96527709
adix67 <adrianek.markowski@gmail.com>
2025-06-10 18:22:05 +0200

    Rozszerzona działalność funkcji zmiany motywu strony

--- skrypt.js
+++ skrypt.js
@@ -1,4 +1,11 @@
-const switchTheme = document.getElementById("story");
-switchTheme.addEventListener("click", () => {
-    document.body.classList.toggle("dark-mode");
+const switchTheme = document.getElementById("theme");
+const main = document.getElementById("main");
+const header = document.getElementById("header");
+const footer = document.getElementById("footer");
+
+switchTheme.addEventListener("click", function() {
+    header.classList.toggle("header");
+    footer.classList.toggle("footer");
+    main.classList.toggle("light-mode");
+    document.body.classList.toggle("normal");
 });
