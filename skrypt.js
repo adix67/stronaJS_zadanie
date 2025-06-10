ba7c042c490034c83b0a3b5d5ae58ab3bdbca669
adix67 <adrianek.markowski@gmail.com>
2025-06-10 19:15:10 +0200

    Transformacje tekstu

--- skrypt.js
+++ skrypt.js
@@ -9,3 +9,32 @@ switchTheme.addEventListener("click", function() {
     main.classList.toggle("light-mode");
     document.body.classList.toggle("normal");
 });
+
+let bolded = document.getElementById("bold");
+let italic = document.getElementById("italic");
+let underline = document.getElementById("underline");
+
+let textTransform = document.getElementById("transformer");
+
+let transform = document.getElementById("text");
+
+transform.addEventListener("click", function() {
+    if (bolded.checked) {
+        textTransform.style.fontWeight = "bold";
+    } else {
+        textTransform.style.fontWeight = "normal";
+    }
+
+    if (italic.checked) {
+        textTransform.style.fontStyle = "italic";
+    } else {
+        textTransform.style.fontStyle = "normal";
+    }
+
+    if (underline.checked) {
+        textTransform.style.textDecoration = "underline";
+    } else {
+        textTransform.style.textDecoration = "none";
+    }
+});
+
